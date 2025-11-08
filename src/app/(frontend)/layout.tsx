6b85ae1d027a84cd8d31e16d82262faf74d3408c
Dan <daniel.rocha8@hotmail.com>
2025-11-08 20:25:21 -0300

    Setup do Projeto

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import './styles.css'
+import { Montserrat } from 'next/font/google'
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
 }
 
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+})
+
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
     <html lang="en">
-      <body>
+      <body className={`${montserrat.className} bg-background min-h-screen`}>
         <main>{children}</main>
       </body>
     </html>
